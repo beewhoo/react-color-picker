c83abe5aebcb0d26941981768c28404e9272a08e
bibek <bibeksinghthapa@gmail.com>
2018-09-24 14:31:32 -0400

    added color component and made changes to the app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Color from './Color'
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      colorsNum: 5,
+      colors: []
+    };
+    for (let i = 0; i < this.state.colorsNum; i +=1) {
+      this.state.colors.push({hexCode: this.generateColor()});
+    }
+  }
+
+  generateColor () {
+    return '#' +  Math.random().toString(16).substr(-6);
+  }
+
+  updateColor (index) {
+    let colors = this.state.colors.slice();
+    const currentColor = this.generateColor();
+    colors[index].hexCode = currentColor;
+    this.setState({
+      colors : colors
+    });
+ }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="color-container">
+        { this.state.colors.map((color, index) => <Color key={`color-${index}`} index={index} update={this.updateColor.bind(this)} hexCode={color.hexCode}></Color>) }
       </div>
-    );
+    )
   }
 }
 
---
+++ src/Color.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Color extends React.Component {
+
+  render () {
+    let style = {
+      backgroundColor: this.props.hexCode
+    }
+    return (
+      <div className="color" onClick={this.props.update.bind(this, this.props.index)} style={style}>
+        <p className="color__code">{this.props.hexCode}</p>
+      </div>
+    );
+  }
+};
+
+export default Color
